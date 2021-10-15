0d49998bbbedeb248c18b5ba09610735b3749c29
doislike <ehdlsgur99@naver.com>
2021-10-15 23:46:54 +0900

    add player animation

--- player_object.py
+++ player_object.py
@@ -5,9 +5,31 @@ class Player:
     def __init__(self):
         self.image = load_image('player.png')
         self.x, self.y = 100, 100
+        self.state = 'idle'
+        self.frame = 0
+        self.rad = 90.0
     def draw(self):
-        self.image.draw(0, 0)
+        self.image.clip_composite_draw(Player.frame * 100, 100 * 1, 100, 100, Player.rad, 'none', Player.x, Player.y, 100, 100)
 
     def update(self):
         self.x = self.x + 1
         self.y = self.y + 1
+
+        #update_animation
+        if(Player.frame > 5):
+            Player.frame = 0
+
+
+    def update_state(moveX = 0, moveY = 0):
+        xDirection = Player.x - moveX
+        yDirection = Player.y - moveY
+        if xDirection < 0:
+            Player.state = 'right'
+        elif xDirection >0:
+            Player.state = 'left'
+        elif xDirection < 0:
+            Player.state = 'left'
+        elif yDirection > 0:
+            Player.state = 'down'
+        elif yDirection < 0:
+            Player.state = 'up'
