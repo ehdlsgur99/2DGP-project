3631be2ad049d47ceb14c961d1a516c3914ab63e
doislike <ehdlsgur99@naver.com>
2021-10-15 23:50:10 +0900

    fix player

--- player_object.py
+++ player_object.py
@@ -9,7 +9,7 @@ class Player:
         self.frame = 0
         self.rad = 90.0
     def draw(self):
-        self.image.clip_composite_draw(Player.frame * 100, 100 * 1, 100, 100, Player.rad, 'none', Player.x, Player.y, 100, 100)
+        self.image.clip_composite_draw(self.frame * 100, 100 * 1, 100, 100, self.rad, 'none', self.x, self.y, 100, 100)
 
     def update(self):
         self.x = self.x + 1
@@ -17,19 +17,19 @@ class Player:
 
         #update_animation
         if(Player.frame > 5):
-            Player.frame = 0
+            self.frame = 0
 
 
-    def update_state(moveX = 0, moveY = 0):
-        xDirection = Player.x - moveX
-        yDirection = Player.y - moveY
+    def update_state(self, moveX = 0, moveY = 0):
+        xDirection = self.x - moveX
+        yDirection = self.y - moveY
         if xDirection < 0:
-            Player.state = 'right'
+            self.state = 'right'
         elif xDirection >0:
-            Player.state = 'left'
+            self.state = 'left'
         elif xDirection < 0:
-            Player.state = 'left'
+            self.state = 'left'
         elif yDirection > 0:
-            Player.state = 'down'
+            self.state = 'down'
         elif yDirection < 0:
-            Player.state = 'up'
+            self.state = 'up'
