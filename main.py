3525ef06cf9f719e2fad455610ba26107af7180b
doislike <ehdlsgur99@naver.com>
2021-10-16 19:08:02 +0900

    attach animation

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from pico2d import *
 import player_object
 
+
 def handle_events():
     global running
     events = get_events()
-    for event in events():
+    for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-
 
 open_canvas()
 
@@ -17,11 +17,14 @@ player = player_object.Player()
 
 running = True
 while running:
+    clear_canvas()
     handle_events()
 
     # Game Logic
-    player.draw()
+    player.update()
+    print(player.x)
     # Game Rendering
+    player.draw()
 
     update_canvas()
     delay(0.05)
--- player_object.py
+++ player_object.py
@@ -3,21 +3,25 @@ from pico2d import *
 
 class Player:
     def __init__(self):
-        self.image = load_image('player.png')
+        self.image = load_image('Resource/Player/Idle.png')
         self.x, self.y = 100, 100
         self.state = 'idle'
         self.frame = 0
-        self.rad = 90.0
+        self.rad = 0.0
     def draw(self):
-        self.image.clip_composite_draw(self.frame * 100, 100 * 1, 100, 100, self.rad, 'none', self.x, self.y, 100, 100)
+        self.image.clip_composite_draw(self.frame * 48, 0, 48, 48, self.rad, 'none', self.x, self.y, 100, 100)
 
     def update(self):
-        self.x = self.x + 1
-        self.y = self.y + 1
-
         #update_animation
-        if(Player.frame > 5):
-            self.frame = 0
+        self.frame = self.frame + 1
+        print(self.frame)
+        if self.state == 'idle':
+            if self.frame > 4:
+                self.frame = 0
+        else:
+            if self.frame > 7:
+                self.frame = 0
+
 
 
     def update_state(self, moveX = 0, moveY = 0):
@@ -25,11 +29,13 @@ class Player:
         yDirection = self.y - moveY
         if xDirection < 0:
             self.state = 'right'
-        elif xDirection >0:
+            self.image = load_image('Resource/Player/RightMove.png')
+        elif xDirection <0:
             self.state = 'left'
-        elif xDirection < 0:
-            self.state = 'left'
+            self.image = load_image('Resource/Player/LeftMove.png')
         elif yDirection > 0:
             self.state = 'down'
+            self.image = load_image('Resource/Player/DownMove.png')
         elif yDirection < 0:
             self.state = 'up'
+            self.image = load_image('Resource/Player/UpMove.png')
