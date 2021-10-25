ed909b3e1333700bdcd002b32b97136ee5b2f0a0
doislike <ehdlsgur99@naver.com>
2021-10-25 23:47:35 +0900

    fix object, add button

--- Button.py
+++ Button.py
@@ -1,5 +1,29 @@
 from pico2d import *
+import object
 
-class Button():
-    def __init__(self):
+class Button(object.obj):
+    def __init__(self,path1, path2, x, y, width, height, frame): # 버튼 이미지 path 두개를 받아옴
+        super().__init__(path1, x, y, width, height, frame)
+        self.isOn = False
+        self.path1 = path1
+        self.path2 = path2
         pass
+
+    def checkPos(self, mouseXPos, mouseYPos):
+        if mouseXPos > self.x and mouseXPos <self.x + self.width and mouseYPos > self.y and mouseYPos < self.y + self.height:
+                if self.isOn == False:
+                    self.image = load_image(self.path2)
+                self.isOn = True
+                return True
+        else:
+            if self.isOn == True
+                self.image = load_image(self.path1)
+            self.isOn = False
+            return False
+
+    def update(self):
+        super().update()
+
+
+
+
--- object.py
+++ object.py
@@ -1,12 +1,12 @@
 from pico2d import *
 
 class obj():
-    def __init__(self, path, x, y, width, height):
+    def __init__(self, path, x, y, width, height, frame):
         self.image = load_image(path)
         self.x, self.y = x,y
         self.width, self.height = width, height
         self.isVisible = True
-        self.frame = 0
+        self.frame = frame
         self.nowFrame = 0
     def render(self):
         self.image.clip_composite_draw(0, 0, self.width, self.height, 0.0, 'none', self.x, self.y)
@@ -16,6 +16,9 @@ class obj():
             self.frame = 0
         else:
             self.frame += 1
+
+    def update(self):
+        self.animation()
 
     def getPos(self):
         return self.x, self.y
