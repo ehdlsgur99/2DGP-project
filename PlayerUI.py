cc1bb1ee184bf8dd5ee624079c792eb4173d6b8e
doislike <ehdlsgur99@naver.com>
2021-11-01 19:41:05 +0900

    add playerUI

---
+++ PlayerUI.py
@@ -0,0 +1,15 @@
+from pico2d import *
+import Button
+
+class PlayerUI():
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+    def release(self):
+        pass
