7b3b022bb2b6f080b56da83c9f54c29a7edbb4ff
doislike <ehdlsgur99@naver.com>
2021-11-08 19:29:12 +0900

    add inven

---
+++ Inventory.py
@@ -0,0 +1,14 @@
+import Button
+
+class Inventory:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+    def release(self):
+        pass
