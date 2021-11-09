ab102464442766f7e6844dd9e4597f7c3b5bef46
doislike <ehdlsgur99@naver.com>
2021-11-09 17:23:00 +0900

    add button callbackfunc

--- Button.py
+++ Button.py
@@ -2,11 +2,12 @@ from pico2d import *
 import object
 
 class Button(object.obj):
-    def __init__(self,path1, path2, x, y, width, height, frame): # 버튼 이미지 path 두개를 받아옴
+    def __init__(self,path1, path2, x, y, width, height, frame, func): # 버튼 이미지 path 두개를 받아옴
         super().__init__(path1, x, y, width, height, frame)
         self.isOn = False
         self.path1 = path1
         self.path2 = path2
+        self.callbackfunc = func()
         pass
 
     def checkPos(self, mouseXPos, mouseYPos):
@@ -14,6 +15,7 @@ class Button(object.obj):
                 if self.isOn == False:
                     self.image = load_image(self.path2)
                 self.isOn = True
+                self.callbackfunc()
                 return True
         else:
             if self.isOn == True:
