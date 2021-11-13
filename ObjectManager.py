f44f63a12ca213771d74fe7f29aa03fac17b51ad
doislike <ehdlsgur99@naver.com>
2021-11-13 23:39:35 +0900

    fix objectmanager

--- ObjectManager.py
+++ ObjectManager.py
@@ -1,11 +1,17 @@
-from pico2d import *
-
-class Object_Manager:
-    objects=[][]
+objects = [[],[]]
 
 
-    def add_object(object):
-        pass
+def add_object(o, layer):
+    objects[layer].append(o)
 
-    def remove_object(object):
-        pass
+
+def add_objects(l, layer):
+    objects[layer] += l
+
+
+def remove_object(o):
+    for i in range(len(objects)):
+        if o in objects[i]:
+            objects[i].remove(o)
+            del o
+            break
