d1e54d18942d77423f7a287ef06dfb41fee071d2
doislike <ehdlsgur99@naver.com>
2021-11-23 22:46:47 +0900

    fix store1

--- Store.py
+++ Store.py
@@ -1,12 +1,20 @@
 import object
+import item
 
 class Store:
-
     def __init__(self):
+        self.items = []
+        pass
+
+    #  아이템 생성
+    def createItem(self):
         pass
 
     def update(self):
-        pass
+        for i in self.items:
+            i.update()
 
     def render(self):
+        for i in self.items:
+            i.update()
         pass
--- item.py
+++ item.py
@@ -1,7 +1,21 @@
-class item:
-    def __init__(self):
-        self.x =0
-        self.y =0
-        pass
+import object
+
+class item():
+
+    def __init__(self, path, x, y, width, height, frame = 0, gold):
+        self.gold = gold
+        self.obj = object.obj(path, x, y, width, height, frame)
+
+
+    def update(self):
+        self.obj.update()
+
+    # 아이템 옆에 얼마 인지도 그려 준다.
+    def render(self):
+        self.obj.draw()
+
+
+
+
 
 
